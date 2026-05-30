36fa31faf46d91273397aa73bc723f395facaee5
1inkun <zxc1830597113@gmail.com>
2026-05-30 18:31:58 +0800

    go - httpserve

---
+++ Go/httpserve/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt";
+	"net/http"
+)
+
+func index (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w,"Hello Web")
+}
+
+func main() {
+	http.HandleFunc("/",index)
+	fmt.Println("Listening on port 8080")
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
